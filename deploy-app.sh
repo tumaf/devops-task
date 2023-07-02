e636ed16eb32ac386dd21b762bcceaf62e4e53c2
tumaf <tumaf2@gmail.com>
2023-07-02 22:07:35 +0300

    fixed typo

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
 echo "Deploying application to your cluster.."
-kubectl deploy -f ./kubernetes/my-env-var-configmap.yaml ./kubernetes/api-server-deployment.yaml ./kubernetes/api-server-service.yaml
+kubectl apply -f ./kubernetes/my-env-var-configmap.yaml ./kubernetes/api-server-deployment.yaml ./kubernetes/api-server-service.yaml
 echo "Application deployed."
