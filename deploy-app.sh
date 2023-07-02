2c4d903b8eeada03a5bf976331b3e3e11d0399ac
tumaf <tumaf2@gmail.com>
2023-07-02 22:09:48 +0300

    missed additional flags for files

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
 echo "Deploying application to your cluster.."
-kubectl apply -f ./kubernetes/my-env-var-configmap.yaml ./kubernetes/api-server-deployment.yaml ./kubernetes/api-server-service.yaml
+kubectl apply -f ./kubernetes/my-env-var-configmap.yaml -f ./kubernetes/api-server-deployment.yaml -f ./kubernetes/api-server-service.yaml
 echo "Application deployed."
